f47d7245f90129e0238dd9f9b1b4bd89d2d3bc9f
i.kłusek <J6ZkqwVUn>
2024-12-21 13:53:33 +0100

    MEC-05-Update test origins && remove other .gitingore files

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,6 +1,8 @@
 const allowedOrigins: string[] = [
   'http://localhost:5173', //VITE, SVELTE - dev
-  'http://localhost:4173', //VITE, SVELTE - prod
+  'http://localhost:5174', //VITE, SVELTE - dev (admin)
+  'http://localhost:4173', //VITE, SVELTE - test prod
+  'http://localhost:4174', //VITE, SVELTE - test prod (admin)
 ];
 
 export default allowedOrigins;
