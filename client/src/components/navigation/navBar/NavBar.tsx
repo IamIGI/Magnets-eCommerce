9cba4b7a0282a9f66e796e73b1c018b104d6f17a
i.kłusek <J6ZkqwVUn>
2024-12-22 16:27:33 +0100

    MEC-28-Added Menu section

--- client/src/components/navigation/navBar/NavBar.tsx
+++ client/src/components/navigation/navBar/NavBar.tsx
@@ -1,12 +1,22 @@
+import StyledIcon from '../styledIcon/StyledIcon';
 import StyledLink from '../styledLink/StyledLink';
 import classes from './NavBar.module.scss';
 
 export const NavBar = () => {
   return (
-    <div className={classes.wrapper}>
-      <StyledLink name="Produkty" url="/" />
-      <StyledLink name="Kontakt" url="/" />
-      <StyledLink name="FAQ" url="/" />
-    </div>
+    <nav className={classes.wrapper}>
+      <button className={classes.logo}>
+        <img src="/logo/logo_black-no_background.png" alt="Logo" />
+      </button>
+      <div className={classes.links}>
+        <StyledLink name="Produkty" url="/" />
+        <StyledLink name="Kontakt" url="/" />
+        <StyledLink name="FAQ" url="/" />
+      </div>
+      <div className={classes.icons}>
+        <StyledIcon src="svg/media/facebook.svg" alt="fb" />
+        <StyledIcon src="svg/media/instagram.svg" alt="ig" />
+      </div>
+    </nav>
   );
 };
