6e7bb269514e231b644780ab62897f009918b1c3
i.kłusek <J6ZkqwVUn>
2025-01-21 17:17:15 +0100

    MEC-123-Added basket api endpoints (controllers, service, router)

---
+++ server/src/routes/api/basket.route.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import basketController from '../../controllers/basket.controller';
+
+const router = express.Router();
+
+router.route('/').post(basketController.add);
+router.route('/:id').get(basketController.getByUserId);
+router.route('/:id').put(basketController.updateByUserId);
+router.route('/:id').delete(basketController.removeByUserId);
+
+export = router;
---
+++ server/src/services/baskets.service.ts
@@ -0,0 +1,83 @@
+import { BasketUpdateData } from '../api/magnetsServer/generated';
+import { DB_COLLECTIONS } from '../config/MongoDBConfig';
+import BasketModel from '../models/Basket.model';
+
+const SERVICE_NAME = 'Basket';
+
+const getByUserId = async (id: string) => {
+  try {
+    const data = await BasketModel.findOne({ userId: id })
+      .populate({
+        path: 'basket.productId',
+        model: DB_COLLECTIONS.Products,
+      })
+      .populate({
+        path: 'basket.priceAndSizesArray.priceAndSizeItemId',
+        model: DB_COLLECTIONS.PricesAndSizes,
+      });
+
+    if (!data) {
+      throw new Error(`${SERVICE_NAME} not found. UserId: ${id}`);
+    }
+
+    return data;
+  } catch (err: any) {
+    console.log(err);
+    throw new Error(`Failed to fetch product categories: ${err.message}`);
+  }
+};
+
+const add = async (data: BasketUpdateData) => {
+  try {
+    const newData = new BasketModel(data);
+    console.log(newData);
+    await newData.save();
+    return getByUserId(data.userId!);
+  } catch (err: any) {
+    console.log(err);
+    throw new Error(`Failed to fetch product categories: ${err.message}`);
+  }
+};
+
+const updateByUserId = async (id: string, data: BasketUpdateData) => {
+  try {
+    return await BasketModel.findOneAndUpdate(
+      { userId: id },
+      { $set: data },
+      { new: true, runValidators: true }
+    )
+      .populate({
+        path: 'basket.productId',
+        model: DB_COLLECTIONS.Products,
+      })
+      .populate({
+        path: 'basket.priceAndSizesArray.priceAndSizeItemId',
+        model: DB_COLLECTIONS.PricesAndSizes,
+      });
+  } catch (err: any) {
+    throw new Error(
+      `Failed to update product category  with ID ${id}:\n ${err.message}`
+    );
+  }
+};
+
+const removeByUserId = async (id: string): Promise<void> => {
+  try {
+    const result = await BasketModel.deleteOne({ userId: id });
+    if (result.deletedCount === 0) {
+      throw new Error('Basket not found');
+    }
+  } catch (err: any) {
+    if (err)
+      throw new Error(
+        `Failed to delete product category with ID ${id}: ${err.message}`
+      );
+  }
+};
+
+export default {
+  getByUserId,
+  add,
+  updateByUserId,
+  removeByUserId,
+};
